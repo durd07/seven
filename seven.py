be68d961de9f48864e0b8615eff4e6c736c28dac
Felix Du <felix.du@nokia-sbell.com>
2021-12-21 12:32:51 +0800

    update

--- seven.py
+++ seven.py
@@ -7,12 +7,12 @@ from io import BytesIO
 
 def to_excel(df):
     output = BytesIO()
-    writer = pd.ExcelWriter(output, engine='openpyxl')
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
     df.to_excel(writer, index=False, sheet_name='杜子期血常规数据统计')
     workbook = writer.book
     worksheet = writer.sheets['杜子期血常规数据统计']
-    #format1 = workbook.add_format({'num_format': '0.00'})
-    #worksheet.set_column('A:A', None, format1)
+    format1 = workbook.add_format({'num_format': '0.00'})
+    worksheet.set_column('A:Z', None, format1)
     writer.save()
     processed_data = output.getvalue()
     return processed_data
